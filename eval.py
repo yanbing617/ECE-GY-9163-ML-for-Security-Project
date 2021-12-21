9d4cfb871718a644f933594ee063f864890e2e43
Williamlxh <861548545@qq.com>
2021-12-21 15:46:20 -0500

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,62 @@
+import json
+
+import keras
+import sys
+import h5py
+import numpy as np
+import tensorflow_model_optimization as tfmot
+
+clean_data_filename = "./data/clean_test_data.h5"
+poisoned_data_filename = "./data/sunglasses_poisoned_data.h5"
+bd_model_filename = "./models/sunglasses_bd_net.h5"
+rp_model_filename = "./models/pruned_model.h5"
+
+def data_loader(filepath):
+    data = h5py.File(filepath, 'r')
+    x_data = np.array(data['data'])
+    y_data = np.array(data['label'])
+    x_data = x_data.transpose((0,2,3,1))
+    return x_data, y_data
+
+
+class RepairModel:
+    def __init__(self, bd_model, rp_model, class_cnt):
+        self.bd_model = bd_model
+        self.rp_model = rp_model
+        self.class_cnt = class_cnt
+
+    def predict(self, x_test):
+        y_bd = np.argmax(self.bd_model.predict(x_test), axis=1)
+        y_rp = np.argmax(self.rp_model.predict(x_test), axis=1)
+        res = []
+        for i in range(len(y_bd)):
+            if y_bd[i] == y_rp[i]:
+                res.append(y_bd[i])
+            else:
+                res.append(self.class_cnt)
+        return np.array(res)
+
+
+def main():
+    Prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+    cl_x_test, cl_y_test = data_loader(clean_data_filename)
+    bd_x_test, bd_y_test = data_loader(poisoned_data_filename)
+
+    bd_model = keras.models.load_model(bd_model_filename)
+    rp_model = keras.models.load_model(rp_model_filename)
+#, custom_objects={"PruneLowMagnitude":Prune_low_magnitude}
+    class_cnt = bd_model.output.shape[1]
+
+    good_net = RepairModel(bd_model, rp_model, class_cnt)
+
+    cl_label_p = good_net.predict(cl_x_test)
+    clean_accuracy = np.mean(np.equal(cl_label_p, cl_y_test)) * 100
+    print('Clean Classification accuracy:', clean_accuracy)
+
+    bd_label_p = good_net.predict(bd_x_test)
+    asr = np.mean(np.equal(bd_label_p, bd_y_test)) * 100
+    print('Attack Success Rate:', asr)
+
+
+if __name__ == '__main__':
+    main()
---
+++ prune.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import h5py
+import numpy as np
+import tensorflow_model_optimization as tfmot
+import tempfile
+
+clean_data_filename = './data/clean_validation_data.h5'
+clean_test_filename = './data/clean_test_data.h5'
+poisoned_data_filename = './data/sunglasses_poisoned_data.h5'
+model_filename = './models/sunglasses_bd_net.h5'
+
+def data_loader(filepath):
+    data = h5py.File(filepath, 'r')
+    x_data = np.array(data['data'])
+    y_data = np.array(data['label'])
+    x_data = x_data.transpose((0,2,3,1))
+    return x_data, y_data
+
+def prune_model(base_model, initial_sparsity, final_sparsity, end_step, log_dir):
+    pruning_params = {
+        'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=initial_sparsity,
+                                                                 final_sparsity=final_sparsity,
+                                                                 begin_step=0,
+                                                                 end_step=end_step)
+    }
+    model_for_pruning = tfmot.sparsity.keras.prune_low_magnitude(base_model, **pruning_params)
+    model_for_pruning.compile(optimizer=base_model.optimizer,
+                              loss=base_model.loss,
+                              metrics=['accuracy'])
+    log_dir = 'log'
+    callbacks = [
+        tfmot.sparsity.keras.UpdatePruningStep(),
+        tfmot.sparsity.keras.PruningSummaries(log_dir=log_dir),
+    ]
+    return model_for_pruning, callbacks
+
+
+
+if __name__ == "__main__":
+    #Congiguration for GPU
+    TF_CONFIG_ = tf.compat.v1.ConfigProto()
+    TF_CONFIG_.gpu_options.allow_growth = True
+    sess = tf.compat.v1.Session(config = TF_CONFIG_)
+    # load data and model
+    x_test, y_test = data_loader(clean_test_filename)
+    x_train, y_train = data_loader(clean_data_filename)
+    x_bd, y_bd = data_loader(poisoned_data_filename)
+    bd_model = tf.keras.models.load_model(model_filename)
+    base_model = tf.keras.models.load_model(model_filename)
+    # Hyper param
+    batch_size = 10
+    epochs = 10
+    validation_split = 0.1
+    # compute end_step
+    num_images = x_train.shape[0] * (1 - validation_split)
+    end_step = np.ceil(num_images / batch_size).astype(np.int32) * epochs
+    # prune
+    pruned_model, callbacks = prune_model(base_model, 0.5, 0.8, end_step, "log")
+    # fit
+    pruned_model.fit(x_train, y_train,
+                        batch_size=batch_size, epochs=epochs, validation_split=validation_split,
+                        callbacks=callbacks)
+    # save the model
+    model_for_export = tfmot.sparsity.keras.strip_pruning(pruned_model)
+    pruned_keras_file = "./models/pruned_model.h5"
+    tf.keras.models.save_model(model_for_export, pruned_keras_file, include_optimizer=False)
+    print('Saved pruned Keras model to:', pruned_keras_file)
